5738920e5f886f33697a85b3545921ec7d198630
Chris Barry <me@chrisb.xyz>
2020-01-04 22:03:56 -0500

    fix: remove useless any

--- src/functions/db/graphql/types/me.ts
+++ src/functions/db/graphql/types/me.ts
@@ -5,104 +5,92 @@ import { limit } from '../args';
 import { getFirstN } from '../utils';
 import { github, job, lastfm, photoBlog, project, resume, skill, twitter } from '.';
 
-interface WithLimitParam {
-  limit?: number;
-}
-
-const simple = {
-  name: {
-    type: GraphQLString,
-    description: 'My Name',
-    resolve: () => info.name,
-  },
-  photo: {
-    type: GraphQLString,
-    description: 'A photo of me',
-    resolve: () => info.photo,
-  },
-  linkedin: {
-    type: new GraphQLObjectType({
-      name: 'Linkedin',
-      description: 'My Linkedin Info',
-      fields: {
-        url: { type: GraphQLString, description: 'My LinkedIn URL', resolve: ({ url }) => url },
-      },
-    }),
-    description: 'My LinkedIn Info',
-    resolve: () => ({ url: info.linkedin }),
-  },
-  email: {
-    type: GraphQLString,
-    description: 'My Email Address',
-    resolve: () => info.email,
-  },
-  employed: {
-    type: GraphQLBoolean,
-    description: 'Do I Have A Job?',
-    resolve: () => info.employed,
-  },
-  bio: {
-    type: GraphQLString,
-    description: 'A Little About Me',
-    resolve: () => info.bio,
-  },
-  interests: {
-    type: GraphQLString,
-    description: "What I'm Interested In",
-    resolve: () => info.interests,
-  },
-};
-
-const complex = {
-  resume: {
-    type: resume,
-    description: 'My Resume',
-    resolve: () => info.resume,
-  },
-  projects: {
-    args: { limit },
-    type: new GraphQLList(project),
-    description: 'Some Recent Projects',
-    resolve: (_: any, { limit: max }: WithLimitParam) => getFirstN(max, info.projects),
-  },
-  jobs: {
-    args: { limit },
-    type: new GraphQLList(job),
-    description: 'My Recent Jobs',
-    resolve: (_: any, { limit: max }: WithLimitParam) => getFirstN(max, info.jobs),
-  },
-  skills: {
-    type: new GraphQLList(skill),
-    description: 'Possible Relevant Skills',
-    resolve: () => info.skills,
-  },
-};
-
-const external = {
-  twitter: {
-    type: twitter,
-    description: 'My Twitter Info',
-    resolve: () => ({ url: info.twitter }),
-  },
-  github: {
-    type: github,
-    description: 'My Github Info',
-    resolve: () => ({ url: info.github }),
-  },
-  lastfm: {
-    type: lastfm,
-    description: 'My Last.FM info',
-    resolve: () => ({ url: info.lastfm }),
-  },
-  photoBlog: {
-    type: photoBlog,
-    description: 'My Photo Blog info',
-    resolve: () => ({ url: info.blog }),
-  },
-};
-
 export default new GraphQLObjectType({
   name: 'Me',
   description: 'About Me',
-  fields: () => ({ ...simple, ...complex, ...external }),
+  fields: () => ({
+    name: {
+      type: GraphQLString,
+      description: 'My Name',
+      resolve: () => info.name,
+    },
+    photo: {
+      type: GraphQLString,
+      description: 'A photo of me',
+      resolve: () => info.photo,
+    },
+    linkedin: {
+      type: new GraphQLObjectType({
+        name: 'Linkedin',
+        description: 'My Linkedin Info',
+        fields: {
+          url: { type: GraphQLString, description: 'My LinkedIn URL', resolve: ({ url }) => url },
+        },
+      }),
+      description: 'My LinkedIn Info',
+      resolve: () => ({ url: info.linkedin }),
+    },
+    email: {
+      type: GraphQLString,
+      description: 'My Email Address',
+      resolve: () => info.email,
+    },
+    employed: {
+      type: GraphQLBoolean,
+      description: 'Do I Have A Job?',
+      resolve: () => info.employed,
+    },
+    bio: {
+      type: GraphQLString,
+      description: 'A Little About Me',
+      resolve: () => info.bio,
+    },
+    interests: {
+      type: GraphQLString,
+      description: "What I'm Interested In",
+      resolve: () => info.interests,
+    },
+    resume: {
+      type: resume,
+      description: 'My Resume',
+      resolve: () => info.resume,
+    },
+    projects: {
+      args: { limit },
+      type: new GraphQLList(project),
+      description: 'Some Recent Projects',
+      resolve: (_, { limit: max }) => getFirstN(max, info.projects),
+    },
+    jobs: {
+      args: { limit },
+      type: new GraphQLList(job),
+      description: 'My Recent Jobs',
+      resolve: (_, { limit: max }) => getFirstN(max, info.jobs),
+    },
+    skills: {
+      type: new GraphQLList(skill),
+      description: 'Possible Relevant Skills',
+      resolve: () => info.skills,
+    },
+    twitter: {
+      type: twitter,
+      description: 'My Twitter Info',
+      resolve: () => ({ url: info.twitter }),
+    },
+    github: {
+      type: github,
+      description: 'My Github Info',
+      resolve: () => ({ url: info.github }),
+    },
+    lastfm: {
+      type: lastfm,
+      description: 'My Last.FM info',
+      resolve: () => ({ url: info.lastfm }),
+    },
+    photoBlog: {
+      type: photoBlog,
+      description: 'My Photo Blog info',
+      resolve: () => ({ url: info.blog }),
+    },
+  }),
 });
