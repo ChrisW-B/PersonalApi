a79fbecb22b3833bdc94840d6a1b053641f043ba
Chris Barry <2490974+ChrisW-B@users.noreply.github.com>
2021-07-18 18:44:11 -0400

    📦 (deps): update apollo api

--- src/functions/graphql-api.ts
+++ src/functions/graphql-api.ts
@@ -1,8 +1,15 @@
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
 import { ApolloServer } from 'apollo-server-lambda';
 
 import schema from './db/graphql';
 
-const server = new ApolloServer({ schema, playground: true, introspection: true });
+const server = new ApolloServer({
+  schema,
+  introspection: true,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-exports.handler = server.createHandler({ cors: { origin: '*', credentials: true } });
+exports.handler = server.createHandler({
+  expressGetMiddlewareOptions: { cors: { origin: '*', credentials: true } },
+});
