d4ad1001189f8c1af1b418ec7bdbc31de8f59b2c
Chris Barry <2490974+ChrisW-B@users.noreply.github.com>
2021-12-19 12:06:21 -0500

    💚 (ci): fix babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,37 @@ const nodeExternals = require('webpack-node-externals');
 module.exports = {
   optimization: { minimize: false },
   externals: [nodeExternals()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-typescript', ['@babel/env', { targets: { node: 'current' } }]],
+            plugins: [
+              '@babel/plugin-proposal-class-properties',
+              '@babel/plugin-transform-object-assign',
+              '@babel/plugin-proposal-object-rest-spread',
+            ],
+            env: {
+              production: {
+                presets: [
+                  [
+                    '@babel/env',
+                    {
+                      targets: { node: 'current' },
+                      forceAllTransforms: true,
+                      useBuiltIns: 'usage',
+                    },
+                  ],
+                ],
+              },
+            },
+          },
+        },
+      },
+    ],
+  },
 };
