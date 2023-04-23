66d75c5af5d6a4e716e68c976710ca5c97628588
Chris Barry <2490974+ChrisW-B@users.noreply.github.com>
2023-04-23 14:44:43 -0400

    just guessing (#503)

--- src/functions/graphql-api.ts
+++ src/functions/graphql-api.ts
@@ -1,11 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 import { ApolloServer } from '@apollo/server';
 import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
-import {
-  handlers,
-  middleware,
-  startServerAndCreateLambdaHandler,
-} from '@as-integrations/aws-lambda';
+import { handlers, startServerAndCreateLambdaHandler } from '@as-integrations/aws-lambda';
 
 import schema from './db/graphql';
 
@@ -17,20 +13,27 @@ const server = new ApolloServer({
 
 const apolloHandler = handlers.createAPIGatewayProxyEventRequestHandler();
 
-const corsMiddleware: middleware.MiddlewareFn<typeof apolloHandler> =
-  // eslint-disable-next-line @typescript-eslint/require-await
-  async () => async (result) => {
-    // eslint-disable-next-line no-param-reassign
-    result.headers = {
-      ...result.headers,
-      'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Headers': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
-      'Access-Control-Allow-Credentials': 'true',
-    };
-    return Promise.resolve();
-  };
-
 export const handler = startServerAndCreateLambdaHandler(server, apolloHandler, {
-  middleware: [corsMiddleware],
+  middleware: [
+    /* eslint-disable @typescript-eslint/require-await,arrow-body-style,no-param-reassign */
+    async (event) => {
+      return async (result) => {
+        event.headers = {
+          ...result.headers,
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Methods': '*',
+          'Access-Control-Allow-Credentials': 'true',
+        };
+        result.headers = {
+          ...result.headers,
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': '*',
+          'Access-Control-Allow-Methods': '*',
+          'Access-Control-Allow-Credentials': 'true',
+        };
+      };
+    },
+    /* eslint-enable @typescript-eslint/require-await,arrow-body-style,no-param-reassign */
+  ],
 });
